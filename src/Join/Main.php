7c24db1db16ef2f045a83253ba7e6c6e7b5f2bda
Sebasfra <109186535+Sebasfra@users.noreply.github.com>
2022-08-01 00:49:27 -0500

    update Main.php

--- src/Join/Main.php
+++ src/Join/Main.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace Join;
+
+use pocketmine\player\Player;
+use pocketmine\event\player\PlayerJoinEvent!
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\Listener;
+
+class Main implements Listener {
+  public function onPlayerJoin(PlayerJoinEvent $event): void {
+	  $player = $event->getPlayer();
+    $nick = $player->getName();
+    $event->setJoinMessage("§l§2[ §a+§7 ] §d$nick §7se a unido al servidor")
+}
