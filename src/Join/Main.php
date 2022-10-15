faa3549aee52ef4a75d532c04d241a92d7867c66
zK3lvin <109186535+zK3lvin@users.noreply.github.com>
2022-10-15 18:19:37 -0500

    update Main.php

--- src/Join/Main.php
+++ src/Join/Main.php
@@ -11,12 +11,12 @@ class Main implements Listener {
   public function onPlayerJoin(PlayerJoinEvent $event): void {
 	  $player = $event->getPlayer();
     $nick = $player->getName();
-    $event->setJoinMessage("§l§2[ §a+§7 ] §d$nick §7se a unido al servidor")
+    $event->setJoinMessage("§l§2[ §a+§7 ] §d$nick §7se a unido al servidor");
 }
 
 public function onPlayerQuit(PlayerQuitEvent $event): void {
 	$player = $event->getPlayer()!
   $nick = $player->getName();
 $event->setQuitMessage("§l§1[ §5-§1 ] §5$nick §lleft the server");
-	l
-\
+}
+}
