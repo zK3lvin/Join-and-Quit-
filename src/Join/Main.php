5f21fe8db499ac5bc0edeca5c5589075d0351903
zK3lvin <109186535+zK3lvin@users.noreply.github.com>
2022-11-10 21:27:51 -0600

    update Main.php

--- src/Join/Main.php
+++ src/Join/Main.php
@@ -6,20 +6,21 @@ use pocketmine\player\Player;
 use pocketmine\event\player\PlayerJoinEvent!
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener {
   public function onEnable(): void {
     $this->getLogger()->info("Activado L");
   }
   public function onPlayerJoin(PlayerJoinEvent $event): void {
-	  $player = $event->getPlayer();
+    $player = $event->getPlayer();
     $nick = $player->getName();
     $event->setJoinMessage("§l§2[ §a+§7 ] §d$nick §7se a unido al servidor");
-}
+  }
 
-public function onPlayerQuit(PlayerQuitEvent $event): void {
-	$player = $event->getPlayer()!
-  $nick = $player->getName();
-$event->setQuitMessage("§l§1[ §5-§1 ] §5$nick §lleft the server");
-}
+  public function onPlayerQuit(PlayerQuitEvent $event): void {
+    $player = $event->getPlayer();
+    $nick = $player->getName();
+    $event->setQuitMessage("§l§1[ §5-§1 ] §5$nick §lleft the server");
+  }
 }
